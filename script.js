215ec0e262d4a717ae13ee087e73c49029ba2e49
ethiccraftorg <ethiccraft.jcbust@gmail.com>
2024-10-29 23:56:07 +0530

    Second Commit

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ document.getElementById('nameForm').addEventListener('submit', function(e) {
     
     // Load your certificate image
     const img = new Image();
-    img.src = 'certificate.jpg'; // Replace with your image path
+    img.src = './images/certificate.jpg'; // Replace with your image path
     img.onload = function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear previous drawings
         ctx.drawImage(img, 0, 0);
-        ctx.font = '30px Arial';
+        ctx.font = '200px monospace';
         ctx.fillStyle = 'black'; // Text color
         ctx.textAlign = 'center';
         ctx.fillText(userName, canvas.width / 2, canvas.height / 2); // Center the text
@@ -24,7 +24,8 @@ document.getElementById('nameForm').addEventListener('submit', function(e) {
 document.getElementById('downloadBtn').addEventListener('click', function() {
     const canvas = document.getElementById('certificateCanvas');
     const link = document.createElement('a');
-    link.download = 'certificate.png';
+    link.download = 'udgaar-certificate.png';
     link.href = canvas.toDataURL();
     link.click();
+    document.getElementById('downloadBtn').style.display = 'none';
 });
